acedc9c4f4e3c6c7836729797c82f84381902307
Daniel Marin <danielarturomt@gmail.com>
2022-05-15 19:58:34 +0100

    feat: add more inputs and logs

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { Connection } = require("@solana/web3.js");
 
-try {
-  // `program-id` input defined in action metadata file
-  const programId = core.getInput('program-id');
-  console.log(`Hello ${programId}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const programId = core.getInput("program-id");
+    const botId = core.getInput("bot-id");
+    const githubRepository = core.getInput("github-repository");
+    const rpcEndpoint = core.getInput("rpc-endpoint");
+    const cluster = core.getInput("cluster");
+    const token = core.getInput("token");
+
+    const [owner, repoName] = githubRepository.split("/");
+    const connection = new Connection(rpcEndpoint);
+    const octokit = github.getOctokit(token);
+
+    console.log(connection)
+
+    const { data: repository } = await appOctokit.repos.get({
+      repo: repoName,
+      owner,
+    });
+
+		console.log(repository)
+
+    core.setOutput("result", true);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
