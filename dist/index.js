481d7ae7d3a462a0a10b14e02f62d3cca04ee090
Daniel Marin <danielarturomt@gmail.com>
2022-05-15 20:41:57 +0100

    feat: add real code to it

--- dist/index.js
+++ dist/index.js
@@ -51969,7 +51969,7 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(3043);
 const github = __nccwpck_require__(8672);
-const { Connection } = __nccwpck_require__(1370);
+const { Connection, PublicKey } = __nccwpck_require__(1370);
 
 async function run() {
   try {
@@ -51984,14 +51984,117 @@ async function run() {
     const connection = new Connection(rpcEndpoint);
     const octokit = github.getOctokit(token);
 
-    console.log(connection)
-
-    const { data: repository } = await appOctokit.repos.get({
+    const { data: repository } = await octokit.repos.get({
       repo: repoName,
       owner,
     });
+    const [boardPublicKey] = await PublicKey.findProgramAddress(
+      [
+        Buffer.from("board", "utf8"),
+        new BN(repository.id).toArrayLike(Buffer, "le", 4),
+      ],
+      new PublicKey(programId)
+    );
 
-		console.log(repository)
+    // get all issues with a bounty enabled
+    const { data: issuesForRepo } = await octokit.issues.listForRepo({
+      repo: repoName,
+      owner,
+      labels: "drill:bounty:enabled",
+      state: "open",
+    });
+  
+    issuesForRepo.forEach(async (issue) => {
+      // find bounty enabled comment
+      const { data: issueComments } = await octokit.issues.listComments({
+        owner,
+        repo: repoName,
+        issue_number: issue.number,
+      });
+  
+      const bountyEnabledComment = issueComments.find((comment) => {
+        return (
+          comment.user?.id.toString() === botId &&
+          comment.body?.toLowerCase().includes("bounty enabled")
+        );
+      });
+  
+      if (bountyEnabledComment !== undefined) {
+        // find bounty vault account
+        const [bountyPublicKey] = await PublicKey.findProgramAddress(
+          [
+            Buffer.from("bounty", "utf8"),
+            boardPublicKey.toBuffer(),
+            new BN(issue.number).toArrayLike(Buffer, "le", 4),
+          ],
+          new PublicKey(programId)
+        );
+        const [bountyVaultPublicKey] = await PublicKey.findProgramAddress(
+          [Buffer.from("bounty_vault", "utf8"), bountyPublicKey.toBuffer()],
+          new PublicKey(programId)
+        );
+  
+        const bountyVaultAccount = await getAccount(
+          connection,
+          bountyVaultPublicKey
+        );
+        const acceptedMint = await getMint(connection, bountyVaultAccount.mint);
+  
+        const tokens = await new TokenListProvider().resolve();
+        const tokenList = tokens.filterByClusterSlug(cluster).getList();
+        const mintDetails = tokenList.find(
+          (token) => token.address === acceptedMint.address.toBase58()
+        );
+  
+        const bodyAsArray = bountyEnabledComment.body
+          ?.split("\n")
+          .filter((segment) => segment !== "");
+  
+        const bountyVaultUserAmount = (
+          Number(bountyVaultAccount.amount) / Math.pow(10, acceptedMint.decimals)
+        ).toLocaleString(undefined, {
+          currencySign: mintDetails?.symbol,
+          minimumFractionDigits: 2,
+        });
+  
+        const explorerUrl = new URL(
+          `https://explorer.solana.com/address/${bountyVaultPublicKey.toBase58()}`
+        );
+  
+        explorerUrl.searchParams.append("cluster", cluster);
+  
+        if (cluster === "custom") {
+          explorerUrl.searchParams.append("customUrl", rpcEndpoint);
+        }
+  
+        let body = "";
+  
+        if (bodyAsArray?.length === 2) {
+          body = [
+            ...bodyAsArray,
+            `Total: ${bountyVaultUserAmount}${
+              mintDetails === undefined ? " (Unknown Token)" : ""
+            } [view in explorer](${explorerUrl.toString()}).`,
+          ].join("\n");
+        } else if (bodyAsArray?.length === 3) {
+          body = [
+            ...bodyAsArray.slice(0, -1),
+            `Total: ${bountyVaultUserAmount}${
+              mintDetails === undefined ? " (Unknown Token)" : ""
+            } [view in explorer](${explorerUrl.toString()}).`,
+          ].join("\n");
+        }
+
+        console.log(body);
+  
+        /* await octokit.issues.updateComment({
+          body,
+          comment_id: bountyEnabledComment.id,
+          owner,
+          repo: repoName,
+        }); */
+      }
+    });
 
     core.setOutput("result", true);
   } catch (error) {
